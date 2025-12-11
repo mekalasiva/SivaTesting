4803e63e014a7f850391ac0fa5203e5feda7b4c5
Siva <mekalasiva73@gmail.com>
2025-12-11 23:35:26 +0530

    Added new file

---
+++ SivaTestResult/SivaTestResult/DashboardSwiftUIView.swift
@@ -0,0 +1,23 @@
+//
+//  DashboardSwiftUIView.swift
+//  SivaTestResult
+//
+//  Created by Siva Mekala on 11/12/25.
+//
+
+import SwiftUI
+
+struct DashboardSwiftUIView: View {
+    var body: some View {
+        VStack {
+            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/).shadow(color: .red, radius: 1)
+
+        }
+        
+    }
+}
+
+#Preview {
+    DashboardSwiftUIView()
+}
