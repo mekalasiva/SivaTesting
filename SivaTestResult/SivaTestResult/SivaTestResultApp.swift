205aa4fdc63bb36826b8e950f363f033266b3ca4
Siva <mekalasiva73@gmail.com>
2025-12-11 23:21:39 +0530

    Addded new changes

---
+++ SivaTestResult/SivaTestResult/SivaTestResultApp.swift
@@ -0,0 +1,17 @@
+//
+//  SivaTestResultApp.swift
+//  SivaTestResult
+//
+//  Created by Siva Mekala on 11/12/25.
+//
+
+import SwiftUI
+
+@main
+struct SivaTestResultApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SivaTestResult/SivaTestResultTests/SivaTestResultTests.swift
@@ -0,0 +1,17 @@
+//
+//  SivaTestResultTests.swift
+//  SivaTestResultTests
+//
+//  Created by Siva Mekala on 11/12/25.
+//
+
+import Testing
+@testable import SivaTestResult
+
+struct SivaTestResultTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
